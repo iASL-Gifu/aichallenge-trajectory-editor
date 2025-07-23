903ff6ae35f620eb9bd6cea8382f39a3845f1cf9
toki-1441 <tokihaya140401@gmail.com>
2025-07-23 18:05:35 +0900

    fix: script

--- editor/shell.sh
+++ editor/shell.sh
@@ -3,4 +3,6 @@
 # ros2 run my_package my_node --ros-args -p my_param:=my_value
 source /opt/ros/humble/setup.bash >/dev/null
 source /aichallenge/workspace/install/setup.bash >/dev/null
-ros2 run csv_editor csv_editor_node --ros-args -p base_path:=$1 >/dev/null
+# ros2 run csv_editor csv_editor_node --ros-args -p base_path:=$1 >/dev/null
+echo "Setting CSV path to $1"
+ros2 param set /planning/scenario_planning/simple_trajectory_generator csv_path $1 >/dev/null
